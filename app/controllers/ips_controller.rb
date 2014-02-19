d0c40e9da58a2a9ee0956fbf96c3e99af18d0054
Adam Louis <adamlouis@gmail.com>
2014-02-19 14:50:23 -0800

    polished slightly

--- app/controllers/ips_controller.rb
+++ app/controllers/ips_controller.rb
@@ -1,6 +1,6 @@
 class IpsController < ApplicationController
   def index
     Visit.create(ip: request.ip, referrer: request.referrer)
-    @ips = IP.all
+    @ips = IP.all unless Rails.env.production?
   end
 end
