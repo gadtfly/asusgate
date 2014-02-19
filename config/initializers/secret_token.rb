defa3cfdd7d289486a75f95e91c6ae6ab973d115
Adam Louis <adamlouis@gmail.com>
2014-02-19 09:46:14 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ASUSGate::Application.config.secret_key_base = 'f0be1db5906be81da923478d48cd2ab5e6d66f01b45a50c483e4475810114dde4164d871ade99dfa05a4b57f2d269a90fbc9ed8cd4f54f52896c041de08f5e2a'
